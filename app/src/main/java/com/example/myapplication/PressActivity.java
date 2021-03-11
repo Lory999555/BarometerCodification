e6cd9f3c0d89d364417beb830fd0e0a0b2cfcd07
Lory999555 <lorenzomorelli95@gmail.com>
2021-03-11 10:25:22 +0100

    implemented time_slot approach with recompute feature based on pivot. implemented logicaRilevamento_3.

--- app/src/main/java/com/example/myapplication/PressActivity.java
+++ app/src/main/java/com/example/myapplication/PressActivity.java
@@ -23,10 +23,8 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.TextView;
 
-import java.sql.Time;
 import java.util.LinkedList;
 import java.util.concurrent.TimeUnit;
 
@@ -48,17 +46,24 @@ public class PressActivity extends Activity {
     private float minVal = Float.MAX_VALUE;
     LinkedList<Float> rilevazioni = new LinkedList<Float>();    // rilevazioni
     private Stato stato = Stato.CAMPIONAMENTO;  //stato dell'app
-    private float offset = 0.005f;   //studiarsi qualcosa per sceglierlo meglio sto valore magari si può ricavare direttamente dallo scostamento con la media
+    private float offset = 0.025f;   //studiarsi qualcosa per sceglierlo meglio sto valore magari si può ricavare direttamente dallo scostamento con la media
     private float pivot;    //media dei rilevamenti (basata sul num_campionamento)
-    private int num_capionamento = 150; //num di campionamenti per decidere il pivot, maxVal e minVal
+    private int num_capionamento = 50; //num di campionamenti per decidere il pivot, maxVal e minVal
     private int num_rilevazioni = 15;   //num necessario per decidere se si è dentro il range o fuori
     private int num_ril_rumore = 4; //sono necessarie un tot di rilevazioni consecutive per azzerare il contatore
     private float data; //variabile che contiene il valore del sensore
     private long nowTime;
     private long limitTime;
-    private boolean reset=true;
-
-
+    private boolean reset_rilevamento = true;
+    private boolean reset_campionamento = true;
+    private int time_slot = 5;
+    private int num_time_slot_before_recompute = 5;
+    private int count_time_slot = 0;
+    private boolean upOrDown; //true se i sensori rilevano un valore oltre la soglia e false se rilevano un valore al di sotto della soglia (pivot)
+    private int countUpDown = 0;
+    private int countPivot = 0;     //logica per resettare il valore del pivot se i rilevamenti sono sempre sopra o sotto il pivot
+    private int limitPivot =  0;      //num di rilevamenti consecutivi chde servono per far scattare il ricalcolo del pivot
+                                    //si potrebbe mettere anche una logica tipo ogni tot time_slot ricalcola il pivot
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,10 +83,16 @@ public class PressActivity extends Activity {
                 String s;
                 switch (stato) {
                     case CAMPIONAMENTO:
+                        if (reset_campionamento) {
+                            nowTime = System.currentTimeMillis();
+                            limitTime = nowTime + TimeUnit.SECONDS.toMillis(time_slot);
+                            reset_campionamento=false;
+
+                        }
                         //aggiornamento della grafica
                         s = Float.toString(data);
                         pressView.setText(s);
-                        hzView.setText("Sto campionando il segnale: " + counter_camp);
+                        hzView.setText("Sto campionando il segnale: \n" + counter_camp);
 
                         //logica del campionamento
                         counter_camp++;
@@ -90,13 +101,14 @@ public class PressActivity extends Activity {
                             maxVal = data;
                         if (minVal > data)
                             minVal = data;
-                        if (counter_camp == num_capionamento) {
+                        nowTime = System.currentTimeMillis();
+                        if (nowTime >= limitTime) {
                             stato = Stato.RILEVAMENTO;
 
                             //calcolaScostamento();
-                            calcolaScostamento_2();
+                            calcolaScostamento_3();
 
-                            titleView.setText("pressure (mbar) \n pivot : " + pivot + "\n maxVal : " + maxVal + "\n minVal : " + minVal);
+                            titleView.setText(" pressure (mbar) \n pivot : " + pivot + "\n maxVal : " + maxVal + "\n minVal : " + minVal);
 
                             //potrei anche usare una sorta di euristica (o media pesata) per scegliere i limiti ad esempio far valore la media 60% e il valore del max e min un tot
                         }
@@ -105,12 +117,20 @@ public class PressActivity extends Activity {
 
 
                     case RILEVAMENTO:
-                        if(reset) {
+                        if (reset_rilevamento) {
                             nowTime = System.currentTimeMillis();
-                            limitTime = nowTime + TimeUnit.SECONDS.toMillis(1);
-                            counter_ril_out=0;
-                            counter_ril_in=0;
-                            reset=false;
+                            limitTime = nowTime + TimeUnit.SECONDS.toMillis(time_slot);
+                            counter_ril_out = 0;
+                            counter_ril_in = 0;
+                            reset_rilevamento = false;
+                            countUpDown = 0;
+                            count_time_slot++;
+                            if (count_time_slot == num_time_slot_before_recompute){
+                                count_time_slot=0;
+                                stato = Stato.CAMPIONAMENTO;
+                                reset_campionamento=true;
+                                counter_camp=0;
+                            }
                         }
                         //stampare dentro il range o fuori dal range se un tot di rilevazioni superano sta cosa dove sto tot è modulare
                         //accuracy
@@ -120,7 +140,7 @@ public class PressActivity extends Activity {
                         pressView.setText(s);
                         //hzView.setText("sto analizzando");
 
-                        logicaRilevamento_2();
+                        logicaRilevamento_3();
                         break;
                 }
             }
@@ -135,22 +155,6 @@ public class PressActivity extends Activity {
     }
 
 
-    //per pulizia del codice futuro
-    //seconda logica implementata
-    private void calcolaScostamento_2() {
-        float sum = 0f;
-        for (float rilevazione : rilevazioni) {
-            sum += rilevazione;
-        }
-        rilevazioni.clear();
-        pivot = sum / num_capionamento;
-        offset = (maxVal - minVal) / 2;
-        minVal = pivot - offset;
-        maxVal = pivot + offset;
-
-        //se rimane questa la logica potrei anche contare il numero di volte che si cambia il massimo ed il minimo e dare più peso ad un o ad un'altro
-
-    }
 
     private void calcolaScostamento() {
         float sum = 0f;
@@ -165,7 +169,46 @@ public class PressActivity extends Activity {
 
     }
 
-    private void logicaRilevamento(){
+
+    //per pulizia del codice futuro
+    //seconda logica implementata
+    private void calcolaScostamento_2() {
+        float sum = 0f;
+        for (float rilevazione : rilevazioni) {
+            sum += rilevazione;
+        }
+        rilevazioni.clear();
+        pivot = sum / num_capionamento;
+        //offset = (maxVal - minVal) / 2;
+        minVal = pivot - offset;
+        maxVal = pivot + offset;
+        if (data > pivot)
+            upOrDown = true;
+        else
+            upOrDown = false;
+        //se rimane questa la logica potrei anche contare il numero di volte che si cambia il massimo ed il minimo e dare più peso ad un o ad un'altro
+
+    }
+
+    private void calcolaScostamento_3() {
+        float sum = 0f;
+        for (float rilevazione : rilevazioni) {
+            sum += rilevazione;
+        }
+        rilevazioni.clear();
+        pivot = sum / counter_camp;
+        //offset = (maxVal - minVal) / 2;
+        minVal = pivot - offset;
+        maxVal = pivot + offset;
+        if (data > pivot)
+            upOrDown = true;
+        else
+            upOrDown = false;
+        //se rimane questa la logica potrei anche contare il numero di volte che si cambia il massimo ed il minimo e dare più peso ad un o ad un'altro
+
+    }
+
+    private void logicaRilevamento() {
         //logica rilevamento
         //per prova voglio che per cambiare stato da modificato a normale servano 10 rilevazioni (dentro o fuori il range) consecutive
         if (data > maxVal || data < minVal) {
@@ -184,7 +227,7 @@ public class PressActivity extends Activity {
 
     }
 
-    private void logicaRilevamento_2(){
+    private void logicaRilevamento_2() {
         //logica rilevamento
         //per prova voglio che per cambiare stato da modificato a normale servano 10 rilevazioni (dentro o fuori il range) consecutive
         if (data > maxVal || data < minVal) {
@@ -194,14 +237,33 @@ public class PressActivity extends Activity {
             counter_ril_in++;
         }
         if (System.currentTimeMillis() > limitTime) {
-            reset=true;
+            reset_rilevamento = true;
             if (counter_ril_out >= counter_ril_in)
                 hzView.setText("segnale modificato");
             else
                 hzView.setText("segnale normale");
         }
-
     }
+
+    //time_slot
+    //dai vari esperimenti si nota come cambia il valore medio della pressione quindi sarebbe buono ricalcolare il pivot dopo un tot di rilevamenti che passano magari i limiti.
+    private void logicaRilevamento_3() {
+        //logica rilevamento
+        //per prova voglio che per cambiare stato da modificato a normale servano 10 rilevazioni (dentro o fuori il range) consecutive
+        if (data > pivot && upOrDown == false) {
+            countUpDown++;
+            upOrDown = true;
+
+        } else if (data < pivot && upOrDown == true) {
+            countUpDown++;
+            upOrDown = false;
+        }
+        if (System.currentTimeMillis() >= limitTime) {
+            reset_rilevamento = true;
+            hzView.setText("countUpDown : " + countUpDown);
+        }
+    }
+
 
     @Override
     protected void onResume() {
